c6dd0f2a52ac7175e87dcfdeb35d1262788e192a
vDescent <timotiusryjaska@gmail.com>
2025-08-04 15:59:24 +0700

    chore: correct vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(),],
+  base: '/', // Penting agar path tetap root
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist' // Vercel default mencari folder ini
+  }
 })
